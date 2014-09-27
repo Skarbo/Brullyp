39305d86a4e587af007414331d6d012b059cec72
Kris Skarbo <kris.skarbo@gmail.com>
2014-09-27 17:12:20 +0200

    Added support for firefox not rotating SVG correctly

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,19 @@ function onLoad() {
         var $loader = document.querySelector( "loader" );
         $loader.classList.add( "hide" );
 
+        var isFirefox = navigator.userAgent.toLowerCase().indexOf( 'firefox' ) > -1,
+            isNoRotate = isFirefox,
+            $objects = document.querySelectorAll( "object" );
+
+        if ( isNoRotate ) {
+            var $object,
+                objectsLength = $objects.length;
+            for ( var i = 0; i < objectsLength; i++ ) {
+                $object = $objects[i];
+                ($object.contentDocument || $object.contentWindow.document).querySelector( "svg" ).classList.add( "no-rotate" );
+            }
+        }
+
         setTimeout( function () {
             $loader.parentNode.removeChild( $loader );
         }, 150 );
